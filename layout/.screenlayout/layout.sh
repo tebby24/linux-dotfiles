4c23b019ca40913810f5330b40ae30718f154c39
tebby24 <enterted@gmail.com>
2024-11-10 15:17:25 -0500

    layout script

--- layout/.screenlayout/layout.sh
+++ layout/.screenlayout/layout.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
-if xrandr | grep -q "^HDMI-3 connected"; then
-	xrandr --output eDP-1 --mode 1366x768 --pos 0x312 --rotate normal --output DP-1 --off --output HDMI-1 --off --output DP-2 --off --output HDMI-2 --primary --mode 1920x1080 --pos 1366x0 --rotate normal
+
+# Check if HDMI-2 is connected
+if xrandr | grep -q "^HDMI-2 connected"; then
+    # Run the display configuration commands only if HDMI-2 is connected
+    xrandr --output eDP-1 --primary --mode 1366x768 --pos 0x312 --rotate normal \
+           --output DP-1 --off \
+           --output HDMI-1 --off \
+           --output DP-2 --off \
+           --output HDMI-2 --mode 1920x1080 --pos 1366x0 --rotate normal
 else
-    xrandr --output eDP-1 --mode 1920x1200
+    echo "HDMI-2 is not connected. Skipping display configuration."
 fi
